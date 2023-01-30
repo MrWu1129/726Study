2813da6517496a371b83c4767eb262674994c32d
MrWu1129 <3085229110@qq.com>
2023-01-30 23:29:27 +0800

    模拟沙箱安全机制

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+/**
+ * 模拟沙箱安全机制
+ */
+public class String {
+    public static void main(String[] args) {
+        System.out.println("**************My String Class**************");
+    }
+}
